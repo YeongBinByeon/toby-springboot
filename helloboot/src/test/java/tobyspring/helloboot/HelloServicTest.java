cfa1ff5e69daf2bc81b1e77042c1668b5b482744
yeongbin <yeongbin6483@gmail.com>
2024-05-29 07:37:45 +0900

    섹션 5 / DI를 잉요한 Decorator, Proxy 패턴

--- helloboot/src/test/java/tobyspring/helloboot/HelloServicTest.java
+++ helloboot/src/test/java/tobyspring/helloboot/HelloServicTest.java
@@ -13,4 +13,14 @@ public class HelloServicTest {
         Assertions.assertThat(ret).isEqualTo("Hello Test");
     }
 
+    @Test
+    void helloDecorator(){
+        HelloDecorator decorator = new HelloDecorator(name -> name);
+
+        String ret = decorator.sayHello("Test");
+
+        Assertions.assertThat(ret).isEqualTo("*Test*");
+
+    }
+
 }
