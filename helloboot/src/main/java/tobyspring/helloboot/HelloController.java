b0a0c7fafe4bd76fe1ef37c2a77e12add7cf4af5
yeongbin <yeongbin6483@gmail.com>
2024-06-10 22:04:18 +0900

    섹션 9 / 리포지토리를 사용하는 HelloService

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -18,4 +18,10 @@ public class HelloController {
         if(name == null || name.trim().length() == 0) throw new IllegalArgumentException();
         return helloService.sayHello(name);
     }
+
+    @GetMapping("/count")
+    public String count(String name){
+        return name + ": " + helloService.countOf(name);
+    }
+
 }
--- helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -4,14 +4,27 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HelloServiceTest {
+
     @Test
     void SimpleHelloService(){
-        SimpleHelloService helloService = new SimpleHelloService();
+        SimpleHelloService helloService = new SimpleHelloService(helloRepositoryStub);
 
         String ret = helloService.sayHello("Test");
 
         Assertions.assertThat(ret).isEqualTo("Hello Test");
     }
+
+    public static final HelloRepository helloRepositoryStub = new HelloRepository() {
+        @Override
+        public Hello findHello(String name) {
+            return null;
+        }
+
+        @Override
+        public void increaseCount(String name) {
+
+        }
+    };
 
     @Test
     void helloDecorator(){
